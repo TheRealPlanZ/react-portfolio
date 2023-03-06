a23c6744f8eb770216583255e9539145b37a5127
Zack Levin <112544104+TheRealPlanZ@users.noreply.github.com>
2023-03-05 19:17:28 -0700

    Update Projects.js
    Fixed image issues

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -9,7 +9,7 @@ function Projects(props) {
       <div className="row">
         <div className="col-md-4">
           <div className="card mb-4">
-            <img src={require(`../assets/${props.image}`)} alt={props.name} className="card-img-top" />
+            <img src={image} alt='Hello Wordl'/>
             <div className="card-body">
               <h5 className="card-title">{props.title}</h5>
               <p className="card-text">{props.topics}</p>
